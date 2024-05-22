2da84eacf31edded3b51297f6f7aa80e213b6a8e
Abdoulahi LO <faarame@gmail.com>
2024-05-22 13:29:39 +0000

    first commit

---
+++ lib/mina/wordpress.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "wordpress/version"
+
+module Mina
+  module Wordpress
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
